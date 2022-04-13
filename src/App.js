3479acb0e746c75890eefd90141ce0c8646048b0
donqlee <donqlee88@gmail.com>
2022-04-14 00:19:26 +0900

    card, button component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import UserInput from './components/UserInput/UserInput';
-import UserList from './components/UserList/UserList';
+import AddUser from './components/Users/AddUser';
+
 
 function App() {
-  const [info, setInfo] = useState([]);
-  const addInfoHandler = (enteredName, enteredAge) => {
-    setInfo(prevInfo => {
-      const updatedInfo = [...prevInfo];
-      updatedInfo.push({ name : enteredName, age : enteredAge, id : Math.random().toString() });
-      return updatedInfo;
-    })
-  }
-  let content = (
-    <p style={{ textAlign : 'center'}}>No user found. Maybe add one?</p>
-  )
-  if(info.length > 0){
-    content = (
-        <UserList items={info}/>
-    )
-  }
+
 
   return (
     <div>
-      <section id="user-form">
-        <UserInput onAddInfo = {addInfoHandler}/>
-      </section>
-      <section id="user">
-        {content}
-      </section>
+      <AddUser />
       
     </div>
   );
